c3e041221bfb94222480c85a016e0dc7aab52b02
Charles <azanux@gmail.com>
2024-01-21 00:26:18 +0100

    update

--- test/WETH9Handler.sol
+++ test/WETH9Handler.sol
@@ -85,6 +85,7 @@ contract WETH9Handler is CommonBase, StdCheats, StdUtils {
     }
 
     function sendFallback(uint256 amount) public createActor countCall("sendFallback") {
+        
         amount = bound(amount, 0, address(this).balance);
 
         _pay(currentActor, amount);
